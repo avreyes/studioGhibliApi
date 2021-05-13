82e64213fbd6ad8139cfeeac23688917f380930c
alyssa reyes <alyssa.va.reyes@gmail.com>
2021-05-12 23:53:42 -0400

    fetch,style

--- scripts.js
+++ scripts.js
@@ -1,33 +1,35 @@
-const baseURL = 'https://ghibliapi.herokuapp.com/films'
+const baseURL = 'https://ghibliapi.herokuapp.com/films';
 
-const searchForm = document.querySelector('form');
-const filmNames = document.querySelector('ul');
-console.log(searchForm, filmNames);
-
-function fetchFilms() {
-    event.preventDefault ();
+function fetchData() {
+    //console.log("START FETCH");
     fetch(baseURL)
-        .then(results => {
-            console.log(results);
-            return results.json();
-        })
-        .then(json => {
-            console.log(json);
-            displayOriginalTitle(json);
-        })
-        .catch(err => console.log(err))
+        .then(response => {
+            console.log(response);
+            return response.json();  
+    })
+        .then(data => {
+        console.log(data);
+        const html = data.map(films => {
+            return `
+            <div class="container">
+            <div class="filmTitles">
+                <p>${films.original_title}
+                <br> ${films.title}
+                <br> Run Time: ${films.running_time}
+                <br> Director: ${films.director}</p> 
+            </div>
+            <div class="about">
+            <p>${films.description}</p>
+            </div>
+            </div>`
+        }).join('');
+        console.log(html);
+        document.querySelector('#title').insertAdjacentHTML('afterbegin', html);
+    }).catch(error => {
+        console.log(error);
+    })
+
 }
 
-function displayTitle(title){
-    console.log("API Response: ", title);
-    title.array.forEach(t => {
-        console.log(t);
-        let title = document.createElement('li');
-        title.innerText = t.name;
-        filmNames.appendChild(title);
-    })
-    let filmTitle = document.createElement('li');
-    let originalTitle = document.createElement('li');
-    let titleRomanised = document.createElement('li');
-    
-}
+
+fetchData();
